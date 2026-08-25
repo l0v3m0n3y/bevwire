89496fd93d06f57f27c100d22e2bb9f3642b4757
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:28:07 +0300

    Add HTTPMethod enum for HTTP request methods

--- src/bevwire.swift
+++ src/bevwire.swift
@@ -20,6 +20,13 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
 
 public class Bevwire {
     private let api = "https://bevwire.com/api"
